f2239af766f268529fba2e64cb7a6c88ed3f8801
alisultani02 <alisultani.373@gmail.com>
2026-06-29 16:19:11 +0430

    Remove: Logo and Name from authNavbar

--- src/components/layout/authNavbar.tsx
+++ src/components/layout/authNavbar.tsx
@@ -1,13 +1,4 @@
-import { Button } from "@/components/ui/button";
-import { Menu } from "lucide-react";
-
-type NavbarProps = {
-  userName?: string;
-  avatarUrl?: string;
-  onMenuClick?: () => void;
-};
-
-export default function Navbar({ onMenuClick }: NavbarProps) {
+export default function Navbar() {
   const CurrentDate = new Date().toLocaleDateString("en-US", {
     weekday: "long",
     day: "2-digit",
@@ -21,21 +12,8 @@ export default function Navbar({ onMenuClick }: NavbarProps) {
     <div className="w-full bg-white">
       {/*Top row */}
       <div className="flex items-center justify-between px-4 py-3">
-        {/*Logo + Name*/}
-        <div className="flex items-center">
-          <img src="/icons/logo.svg" alt="Logo" className="w-7 h-7" />
-          <span className="-ml-1 text-lg font-semibold p-2">APOS</span>
-        </div>
-
         {/*Right Side */}
-        <div className="flex items-center gap-2">
-          {/* Notification */}
-
-          {/*Menu*/}
-          <Button variant="outline" size="icon" onClick={onMenuClick}>
-            <Menu className="w-5 h-5" />
-          </Button>
-        </div>
+        <div className="flex items-center gap-2">{/* Notification */}</div>
       </div>
 
       {/* Bottom Row */}
--- src/lib/password.ts
+++ src/lib/password.ts
@@ -5,4 +5,4 @@ export const passwordSchema = z
   .min(8, "Password must be at least 8 characters")
   .regex(/[A-Z]/, "Password must contain one uppercase letter")
   .regex(/[a-z]/, "Password must contain one lowercase letter")
-  .regex(/[^A-Za-z0-9]/, "Password must contain one symbol i.e (&,$)");
+  .regex(/[^A-Za-z0-9]/, "Password must contain one symbol i.e (&, @, $)");
